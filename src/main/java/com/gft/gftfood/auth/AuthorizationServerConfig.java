c46200d1984b065cca676a6e5eff169d9b7acbe4
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-30 18:58:22 -0300

    22.23
    Implementando o suporte a PKCE com o fluxo Authorization Code

--- src/main/java/com/gft/gftfood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/gft/gftfood/auth/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.gft.gftfood.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+
+import lombok.var;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,61 +23,55 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients
-		.inMemory()
-			.withClient("gftfood-web")
-			.secret(passwordEncoder.encode("web123"))
-			.authorizedGrantTypes("password","refresh_token")
-			.scopes("write", "read")
-			.accessTokenValiditySeconds(6 * 60 * 60)
-			.refreshTokenValiditySeconds( 60 * 24 * 60 * 60)
-		.and()
-			.withClient("webadmin")
-			.authorizedGrantTypes("implicit")
-			.scopes("write","read")
-			.redirectUris("http://aplicacao-cliente")
-			//http://localhost:8081/oauth/authorize?response_type=token&client_id=webadmin&state=abc&redirect_uri=http://aplicacao-cliente
-			
-		.and()
-			.withClient("foodanalitics")
-			.secret(passwordEncoder.encode("food123"))
-			.authorizedGrantTypes("authorization_code")
-			.scopes("write","read")
-			.redirectUris("http://aplicacao-cliente")
-			//http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalitics&state=abc&redirect_uri=http://aplicacao-cliente
-		.and()
-			.withClient("faturamento")
-			.secret(passwordEncoder.encode("faturamento123"))
-			.authorizedGrantTypes("client_credentials")
-			.scopes("write", "read")
-		.and()
-			.withClient("checktoken")
-			.secret(passwordEncoder.encode("check123"));
-		
-		
+		clients.inMemory().withClient("gftfood-web").secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token").scopes("write", "read")
+				.accessTokenValiditySeconds(6 * 60 * 60).refreshTokenValiditySeconds(60 * 24 * 60 * 60)
+				.and()
+				.withClient("webadmin").authorizedGrantTypes("implicit").scopes("write", "read")
+				.redirectUris("http://aplicacao-cliente")
+				// http://localhost:8081/oauth/authorize?response_type=token&client_id=webadmin&state=abc&redirect_uri=http://aplicacao-cliente
+				.and()
+				.withClient("foodanalitics").secret(passwordEncoder.encode("food123"))
+				.authorizedGrantTypes("authorization_code").scopes("write", "read")
+				.redirectUris("http://aplicacao-cliente")
+				// http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalitics&state=abc&redirect_uri=http://aplicacao-cliente
+				.and()
+				.withClient("faturamento").secret(passwordEncoder.encode("faturamento123"))
+				.authorizedGrantTypes("client_credentials").scopes("write", "read").and().withClient("checktoken")
+				.secret(passwordEncoder.encode("check123"));
+
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		//security.checkTokenAccess("isAuthenticated()")
+		// security.checkTokenAccess("isAuthenticated()")
 		security.checkTokenAccess("permitAll()");
-	
+
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-		.authenticationManager(authenticationManager)
-		.userDetailsService(userDetailsService)
-		.reuseRefreshTokens(false);
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService)
+				.reuseRefreshTokens(false)
+				.tokenGranter(tokenGranter(endpoints));
+	}
+
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+
+		var granters = Arrays.asList(pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+		return new CompositeTokenGranter(granters);
 	}
 }
