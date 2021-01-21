678d59d55e9681c97d07b3f8772de5662d1676bf
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-01-20 21:00:30 -0300

    23.36
    Configurando os clientes OAuth2 em um banco de dados SQL

--- src/main/java/com/gft/gftfood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/gft/gftfood/auth/core/AuthorizationServerConfig.java
@@ -2,13 +2,14 @@ package com.gft.gftfood.auth.core;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -30,9 +31,6 @@ import lombok.var;
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-
-	@Autowired
 	private UserDetailsService userDetailsService;
 
 	@Autowired
@@ -40,69 +38,43 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 //	@Autowired
 //	private RedisConnectionFactory redisConnectionFactory;
-	
+
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties;
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("gftfood-web").secret(passwordEncoder.encode("web123"))
-				.authorizedGrantTypes("password", "refresh_token").scopes("WRITE", "READ")
-				.accessTokenValiditySeconds(6 * 60 * 60).refreshTokenValiditySeconds(60 * 24 * 60 * 60).and()
-				.withClient("webadmin").authorizedGrantTypes("implicit").scopes("WRITE", "READ")
-				.redirectUris("http://aplicacao-cliente")
-				// http://localhost:8081/oauth/authorize?response_type=token&client_id=webadmin&state=abc&redirect_uri=http://aplicacao-cliente
-
-				.and().withClient("foodanalitics").secret(passwordEncoder.encode(""))
-				.authorizedGrantTypes("authorization_code").scopes("WRITE", "READ")
-				.redirectUris("http://www.foodanalitics.local:8082")
-				// http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalitics&redirect_uri=http://www.foodanalitics.local:8082&code_challenge=teste123&code_challenge_method=plain
-				// codeVerifier:test123
-				// codeChalenge:test123
-
-				// http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalitics&redirect_uri=http://www.foodanalitics.local:8082&code_challenge=7_gzRZ-yfzhZ8gH8ljPTzxZXGBn3sC4iRAXqRbCN5mw&code_challenge_method=s256
-				// codeVerifier:iwdcfbuwefcbubfuberufbuebfeouqwbeeflkjwbecdqeifbqwhefbhjbef
-				// codigo para s256 e baseUrl
-				// codeChalenge:7_gzRZ-yfzhZ8gH8ljPTzxZXGBn3sC4iRAXqRbCN5mw com sha256 e baseUrl
-
-				.and().withClient("webadmin").authorizedGrantTypes("implicit").scopes("WRITE", "READ")
-				.redirectUris("http://aplicacao-cliente")
-				// http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalitics&state=abc&redirect_uri=http://aplicacao-cliente
-
-				.and().withClient("faturamento").secret(passwordEncoder.encode("faturamento123"))
-				.authorizedGrantTypes("client_credentials").scopes("WRITE", "READ").and().withClient("checktoken")
-				.secret(passwordEncoder.encode("check123"));
+		clients.jdbc(dataSource);
 
 	}
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		// security.checkTokenAccess("isAuthenticated()")
-		security.checkTokenAccess("permitAll()")
-		.tokenKeyAccess("permitAll()")
-		.allowFormAuthenticationForClients();
+		security.checkTokenAccess("permitAll()").tokenKeyAccess("permitAll()").allowFormAuthenticationForClients();
 
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		
+
 		var enhancerChain = new TokenEnhancerChain();
-		enhancerChain.setTokenEnhancers(
-				Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
-		
+		enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+
 		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService)
 				.reuseRefreshTokens(false).accessTokenConverter(jwtAccessTokenConverter())
 				// .tokenStore(redisTokenStore())
-				.tokenEnhancer(enhancerChain)
-				.approvalStore(approvalStore(endpoints.getTokenStore()))
+				.tokenEnhancer(enhancerChain).approvalStore(approvalStore(endpoints.getTokenStore()))
 				.tokenGranter(tokenGranter(endpoints));
 	}
-	
+
 	private ApprovalStore approvalStore(TokenStore tokenStore) {
 		var approvalStore = new TokenApprovalStore();
 		approvalStore.setTokenStore(tokenStore);
-		
+
 		return approvalStore;
 	}
 
@@ -113,11 +85,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		var jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		
-		//chave simetrica
+
+		// chave simetrica
 		// jwtAccessTokenConverter.setSigningKey("gftworkswifbqweifwefweofoiwehfoihweoifhoihewfiohwefoejfnonfonrfnnrfnfeon3fon3o3lnefon3eef");
 
-		//chave assimetrica
+		// chave assimetrica
 		var jksResource = new ClassPathResource(jwtKeyStoreProperties.getPath());
 		var keyStorePass = jwtKeyStoreProperties.getPassword();
 		var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
